df3e38f5b80d28b06d2c5f6064548c5b9bb77d0a
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-03-18 09:33:21 +0100

    Create Toolbox ADO (Unfinish)

---
+++ Toolbox/Database/Connect.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Text;
+
+namespace Toolbox.Database
+{
+    public sealed class Connect
+    {
+        private string _ConnectionString;
+        private DbProviderFactory _Factory;
+
+        public Connect(string connectionString)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString))
+                throw new ArgumentNullException(nameof(connectionString));
+
+            _ConnectionString = connectionString;
+            _Factory = DbProviderFactories.GetFactory("System.Data.SqlClient");
+        }
+
+        private DbCommand CreateCommand(DbConnection db, Query query)
+        {
+            DbCommand command = db.CreateCommand();
+
+            command.CommandText = query.Request;
+            command.CommandType = query.IsProcedure ? CommandType.StoredProcedure : CommandType.Text;
+
+            foreach (KeyValuePair<string, object> parameter in query.Parameters)
+            {
+                DbParameter p = _Factory.CreateParameter();
+                p.ParameterName = parameter.Key;
+                p.Value = parameter.Value;
+
+                command.Parameters.Add(p);
+            }
+
+            return command;
+        }
+
+        private DbConnection CreateConnection()
+        {
+            DbConnection db = _Factory.CreateConnection();
+            db.ConnectionString = _ConnectionString;
+            return db;
+        }
+
+
+        public int ExecuteNonquery(Query query) 
+        {
+            using (DbConnection db = CreateConnection())
+            {
+                using(DbCommand cmd = CreateCommand(db, query))
+                {
+                    db.Open();
+
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public TResult ExecuteScalar<TResult>(Query query)
+        {
+            using (DbConnection db = CreateConnection())
+            {
+                using (DbCommand cmd = CreateCommand(db, query))
+                {
+                    db.Open();
+
+                    object o = cmd.ExecuteNonQuery();
+
+                    return (o is DBNull) ? default(TResult) : (TResult)o;
+                }
+            }
+        }
+
+        // TODO Finish this  ;)
+        // public ... ExecuteReader() { }
+
+    }
+}
---
+++ Toolbox/Database/Query.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Toolbox.Database
+{
+    public sealed class Query
+    {
+        public string Request { get; }
+        public bool IsProcedure { get; }
+
+        private Dictionary<string, object> _Parameters;
+        public IEnumerable<KeyValuePair<string, object>> Parameters
+        {
+            get { return _Parameters.Select(p => p); }
+        }
+
+        public Query(string request, bool isProcedure = false)
+        {
+            if (string.IsNullOrWhiteSpace(request))
+                throw new ArgumentNullException(nameof(request));
+
+            Request = request;
+            IsProcedure = isProcedure;
+            _Parameters = new Dictionary<string, object>();
+        }
+
+        public void AddParameter(string key, object value)
+        {
+            if (string.IsNullOrWhiteSpace(key))
+                throw new ArgumentNullException(nameof(key));
+
+            _Parameters.Add(key, value ?? DBNull.Value);
+        }
+
+    }
+}
