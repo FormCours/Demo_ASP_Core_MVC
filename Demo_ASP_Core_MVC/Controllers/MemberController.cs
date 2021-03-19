7580d2d223eee9e305e17cdab9eaaf625089f86e
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2021-03-19 18:44:22 +0100

    Add Login Page

---
+++ Demo_ASP_Core_MVC/Controllers/MemberController.cs
@@ -0,0 +1,61 @@
+﻿using Demo_ASP_Core_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_ASP_Core_MVC.Controllers
+{
+    public class MemberController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(MemberLoginViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                // TODO Utiliser les data services pour valider le login
+
+                // TODO Si OK - Créer une session pour l'utilisateur
+                // TODO Sinon - Renvoyer l'utilisateur sur la page
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(MemberRegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO Créer un nouveau compte avec les data services
+
+                // TODO Créer une session pour l'utilisateur
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Demo_ASP_Core_MVC/Models/MemberViewModel.cs
+++ Demo_ASP_Core_MVC/Models/MemberViewModel.cs
@@ -2,11 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace Demo_ASP_Core_MVC.Models
 {
-    public class MemberViewModel
+    public class MemberLoginViewModel
     {
+        [Required(AllowEmptyStrings =false, ErrorMessage ="L'adresse E-mail est necessaire !")]
+        [DisplayName("E-mail")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "L'adresse E-mail est invalide")]
+        public string Email { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le mot de passe est necessaire !")]
+        [DisplayName("Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+
+    public class MemberRegisterViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "L'adresse E-mail est necessaire !")]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le pseudo n'est pas en option :p")]
+        [DisplayName("Pseudo")]
+        public string Pseudo { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le mot de passe est requis")]
+        [DisplayName("Mot de passe")]
+        public string Password1 { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La validation du mot de passe est requis")]
+        [Compare(nameof(Password1), ErrorMessage = "Les 2 mots de passe ne sont pas identique !")]
+        [DisplayName("Validation du mot de passe")]
+        public string Password2 { get; set; }
     }
 }
